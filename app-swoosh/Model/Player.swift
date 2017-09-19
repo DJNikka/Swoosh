929f37fe24553e33e9bea70b976baee7345e1506
Nikka-Sher <npiterman@gmail.com>
2017-09-19 02:02:43 -0700

    button only enabled when desired league selected

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Konstantine Piterman on 9/19/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
